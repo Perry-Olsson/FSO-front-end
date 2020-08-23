733a17caba6718a26fc821e6db9a5a638c68ca7e
Perry <olsson.perry@gmail.com>
2020-08-22 18:20:53 -0700

    unicafe

--- part6/unicafe-redux/src/App.js
+++ part6/unicafe-redux/src/App.js
@@ -6,16 +6,16 @@ const App = () => {
     const dispatch = useDispatch()
     const votes = useSelector(state => state)
 
-    const send = (type) => {
+    const vote = (type) => {
         dispatch({ type })
     }
 
     return (
         <div>
-          <button onClick={() => send('GOOD')}>good</button> 
-          <button onClick={() => send('OK')}>neutral</button> 
-          <button onClick={() => send('BAD')}>bad</button>
-          <button onClick={() => send('ZERO')}>reset stats</button>
+          <button onClick={() => vote('GOOD')}>good</button> 
+          <button onClick={() => vote('OK')}>neutral</button> 
+          <button onClick={() => vote('BAD')}>bad</button>
+          <button onClick={() => vote('ZERO')}>reset stats</button>
           <div>good {votes.good}</div>
           <div>neutral {votes.ok}</div>
           <div>bad {votes.bad}</div>
